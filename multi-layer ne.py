38cb25bbd147070136a5b1cf2f4151f78b9c5ef9
Dhyuti <55708942+Dhyuti@users.noreply.github.com>
2023-07-31 18:36:14 +0530

    Create multi-layer ne.py

---
+++ multi-layer ne.py
@@ -0,0 +1,61 @@
+import sys
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+# This script requires TensorFlow 2 and Python 3.
+if tf.__version__.split('.')[0] != '2':
+    raise Exception((f"The script is developed and tested for tensorflow 2. "
+                     f"Current version: {tf.__version__}"))
+
+if sys.version_info.major < 3:
+    raise Exception((f"The script is developed and tested for Python 3. "
+                     f"Current version: {sys.version_info.major}"))
+
+
+my_layer = keras.layers.Dense(units=1, input_shape=[1])
+model = tf.keras.Sequential([my_layer])
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+xs = np.array([-1.0,  0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
+
+model.fit(xs, ys, epochs=500)
+
+print(model.predict([10.0]))
+
+print(my_layer.get_weights())
+
+my_layer_1 = keras.layers.Dense(units=2, input_shape=[1])
+my_layer_2 = keras.layers.Dense(units=1)
+model = tf.keras.Sequential([my_layer_1, my_layer_2])
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+xs = np.array([-1.0,  0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
+
+model.fit(xs, ys, epochs=500)
+
+print(model.predict([10.0]))
+print(my_layer_1.get_weights())
+print(my_layer_2.get_weights())
+
+value_to_predict = 10.0
+
+layer1_w1 = (my_layer_1.get_weights()[0][0][0])
+layer1_w2 = (my_layer_1.get_weights()[0][0][1])
+layer1_b1 = (my_layer_1.get_weights()[1][0])
+layer1_b2 = (my_layer_1.get_weights()[1][1])
+
+
+layer2_w1 = (my_layer_2.get_weights()[0][0])
+layer2_w2 = (my_layer_2.get_weights()[0][1])
+layer2_b = (my_layer_2.get_weights()[1][0])
+
+neuron1_output = (layer1_w1 * value_to_predict) + layer1_b1
+neuron2_output = (layer1_w2 * value_to_predict) + layer1_b2
+
+neuron3_output = (layer2_w1 * neuron1_output) + (layer2_w2 * neuron2_output) + layer2_b
+
+print(neuron3_output)
