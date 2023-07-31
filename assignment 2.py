e12af9bbe0ab7588f3e6b6d287282262f924465b
Dhyuti <55708942+Dhyuti@users.noreply.github.com>
2023-07-31 19:02:42 +0530

    Update assignment 2.py

--- assignment 2.py
+++ assignment 2.py
@@ -12,5 +12,89 @@ model = tf.keras.models.Sequential([tf.keras.layers.Flatten(input_shape=(28,28))
                                     tf.keras.layers.Dense(512, activation=tf.nn.relu), 
                                     tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
 
-training_images  = #YOUR CODE HERE#
-test_images = #YOUR CODE HERE#
+# PLEASE COMPLETE THIS CELL
+# training_images  = YOUR CODE HERE#
+# test_images = YOUR CODE HERE#
+
+# Solution
+
+training_images  = training_images / 255.0
+test_images = test_images / 255.0
+
+# compile the model
+model.compile(optimizer = tf.keras.optimizers.Adam(),
+              loss = 'sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# fit the model to the training data
+model.fit(training_images, training_labels, epochs=5)
+
+# test the model on the test data
+model.evaluate(test_images, test_labels)
+
+classifications = model.predict(test_images)
+print(classifications[0])
+
+# NUMBER_OF_NEURONS = YOUR_CODE_HERE#
+
+# Solution 
+NUMBER_OF_NEURONS = 1024
+
+# define the new model
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(input_shape=(28,28)),
+                                    tf.keras.layers.Dense(NUMBER_OF_NEURONS, activation=tf.nn.relu),
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+# compile fit and evaluate the model again
+model.compile(optimizer = tf.keras.optimizers.Adam(),
+              loss = 'sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(training_images, training_labels, epochs=5)
+model.evaluate(test_images, test_labels)
+
+YOUR_NEW_LAYER = tf.keras.layers.Dense(512, activation=tf.nn.relu)
+
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(input_shape=(28,28)),
+                                    tf.keras.layers.Dense(512, activation=tf.nn.relu),
+                                    YOUR_NEW_LAYER,
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+# compile fit and evaluate the model again
+model.compile(optimizer = tf.keras.optimizers.Adam(),
+              loss = 'sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(training_images, training_labels, epochs=5)
+model.evaluate(test_images, test_labels)
+
+# get new non-normalized mnist data
+training_images_non = training_images * 255
+test_images_non = test_images * 255
+
+# re-compile, re-fit and re-evaluate
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(input_shape=(28,28)),
+                                    tf.keras.layers.Dense(512, activation=tf.nn.relu),
+                                    YOUR_NEW_LAYER,
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+model.compile(optimizer = tf.keras.optimizers.Adam(),
+              loss = 'sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(training_images_non, training_labels, epochs=5)
+model.evaluate(test_images_non, test_labels)
+classifications = model.predict(test_images_non)
+
+# define and instantiate your custom Callback
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('accuracy')>0.86):
+      self.model.stop_training = True
+callbacks = myCallback()
+
+# re-compile, re-fit and re-evaluate
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(input_shape=(28,28)),
+                            tf.keras.layers.Dense(512, activation=tf.nn.relu),
+                            YOUR_NEW_LAYER,
+                            tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+model.compile(optimizer = tf.keras.optimizers.Adam(),
+      loss = 'sparse_categorical_crossentropy',
+      metrics=['accuracy'])
+model.fit(training_images, training_labels, epochs=5, callbacks=[callbacks])
